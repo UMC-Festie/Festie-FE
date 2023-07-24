6fdc024d71990ecc28732b6e4fe3013b05b7b80f
unknown <sohee5143@naver.com>
2023-07-24 14:58:54 +0900

    [Feat] 헤더 푸터 완성

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { Reset } from 'styled-reset'
+import { Reset } from "styled-reset";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Main from "./pages/Main"
-import View from "./pages/View"
+import Main from "./pages/Main";
+import View from "./pages/View";
 import Share from "./pages/Share";
 import Together from "./pages/Together";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Reset />
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/view" element={<View />} />
         <Route path="/share" element={<Share />} />
+        <Route path="/together" element={<Together />} />
         <Route path="/together" element={<Together />} />
       </Routes>
       <Footer />
