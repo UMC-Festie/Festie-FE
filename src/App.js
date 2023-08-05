0ed16ba3b0a21e2f83e13c65c4732bf855263876
unknown <sohee5143@naver.com>
2023-08-05 09:24:58 +0900

    [Feat] 디테일 페이지 ui 수정 중

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Main from "./pages/Main";
 import View from "./pages/View";
 import Share from "./pages/Share";
 import Together from "./pages/Together";
-
+import PerformanceDetail from "./pages/PerformanceDetail";
 function App() {
   return (
     <>
@@ -17,6 +17,10 @@ function App() {
         <Route path="/view" element={<View />} />
         <Route path="/share" element={<Share />} />
         <Route path="/together" element={<Together />} />
+        <Route
+          path="/share/performance_detail"
+          element={<PerformanceDetail />}
+        />
       </Routes>
       <Footer />
     </>
