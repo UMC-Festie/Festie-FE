4c4a160c3820830eae7fdf19c799abae1c244fec
unknown <sohee5143@naver.com>
2023-08-20 18:20:32 +0900

    [fix] 헤더 공연 축제 탭 추가 및 푸터 오류 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,32 +2,105 @@ import styled from "styled-components";
 import Logo from "../assets/festie_logo.png";
 import SearchIcon from "../assets/search.svg";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 export default function Header() {
+  const [isViewHovering, setIsViewHovering] = useState(0);
+  const [isShareHovering, setIsShareHovering] = useState(0);
+  const [isTogetherHovering, setIsTogetherHovering] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+
   const activeStyle = {
-    marginTop: "9px",
-    paddingBottom: "6px",
     borderBottom: "3px solid #FF7A00",
   };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleIconClick = () => {
+    console.log(inputValue);
+  };
+
   return (
-    <HeaderWrap>
-      <NavLink to="/">
-        <LogoImage src={Logo} alt="Logo"></LogoImage>
-      </NavLink>
-      <Menu to="/view/performance" style={({ isActive }) => (isActive ? activeStyle : {})}>
-        정보 보기
-      </Menu>
-      <Menu to="/share/performance" style={({ isActive }) => (isActive ? activeStyle : {})}>
-        정보 공유
-      </Menu>
-      <Menu
-        to="/together"
-        style={({ isActive }) => (isActive ? activeStyle : {})}
-      >
-        같이 가요
-      </Menu>
-      <Icon src={SearchIcon} alt="Search Icon"></Icon>
-      <MyButton to="/mypage"> My Page</MyButton>
+    <HeaderWrap
+      isViewHovering={isViewHovering}
+      isShareHovering={setIsShareHovering}
+      isTogetherHovering={setIsTogetherHovering}
+    >
+      <MenuWrap>
+        <NavLink to="/">
+          <LogoImage src={Logo} alt="Logo"></LogoImage>
+        </NavLink>
+        <Menu
+          to="/view/performance"
+          style={({ isActive }) =>
+            isActive || isViewHovering ? activeStyle : {}
+          }
+          onMouseOver={() => setIsViewHovering(1)}
+          onMouseOut={() => setIsViewHovering(0)}
+        >
+          정보 보기
+        </Menu>
+        <Menu
+          to="/share/performance"
+          style={({ isActive }) =>
+            isActive || isShareHovering ? activeStyle : {}
+          }
+          onMouseOver={() => setIsShareHovering(1)}
+          onMouseOut={() => setIsShareHovering(0)}
+        >
+          정보 공유
+        </Menu>
+        <Menu
+          to="/together"
+          style={({ isActive }) =>
+            isActive || isTogetherHovering ? activeStyle : {}
+          }
+          onMouseOver={() => setIsTogetherHovering(1)}
+          onMouseOut={() => setIsTogetherHovering(0)}
+        >
+          같이 가요
+        </Menu>
+        <SearchBox>
+          <SearchInput
+            value={inputValue}
+            onChange={handleInputChange}
+          ></SearchInput>
+          <Icon
+            src={SearchIcon}
+            alt="Search Icon"
+            onClick={handleIconClick}
+          ></Icon>
+        </SearchBox>
+        <MyButton to="/mypage"> My Page</MyButton>
+      </MenuWrap>
+      <SubMenuWrap>
+        {isViewHovering ? (
+          <SubMenuView
+            onMouseOver={() => setIsViewHovering(1)}
+            onMouseOut={() => setIsViewHovering(0)}
+          >
+            <SubMenuText to="/view/performance">공연</SubMenuText>
+            <SubMenuText to="/view/festival">축제</SubMenuText>
+          </SubMenuView>
+        ) : (
+          ""
+        )}
+        {isShareHovering ? (
+          <SubMenuShare
+            onMouseOver={() => setIsShareHovering(1)}
+            onMouseOut={() => setIsShareHovering(0)}
+          >
+            <SubMenuText to="/share/performance">공연</SubMenuText>
+            <SubMenuText to="/share/festival">축제</SubMenuText>
+            <SubMenuText to="/share/review">후기</SubMenuText>
+            <SubMenuText to="/share/ticketing">티켓팅</SubMenuText>
+          </SubMenuShare>
+        ) : (
+          ""
+        )}
+      </SubMenuWrap>
     </HeaderWrap>
   );
 }
@@ -35,7 +108,6 @@ export default function Header() {
 const HeaderWrap = styled.div`
   display: flex;
   flex-wrap: wrap;
-  align-items: center;
   padding: 24px 90px;
   border-bottom: 1px solid var(--festie-gray-200, #e8e8e8);
   background: #fff;
@@ -47,6 +119,10 @@ const LogoImage = styled.img`
   margin-right: 86px;
 `;
 
+const MenuWrap = styled.div`
+  display: flex;
+  width: 100%;
+`;
 const Menu = styled(NavLink)`
   color: var(--festie-gray-800, #3a3a3a);
   font-size: 16px;
@@ -54,12 +130,80 @@ const Menu = styled(NavLink)`
   font-weight: 500;
   line-height: normal;
   margin: 0px 35px 0px 0px;
+  padding: 9px 0px;
   cursor: pointer;
   text-decoration: none;
+  color: var(--festie-gray-800, #3a3a3a);
+  font-family: SUIT Variable;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
 `;
 
-const Icon = styled.img`
+const SubMenuWrap = styled.div`
+  display: flex;
+`;
+
+const SubMenuView = styled.div`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding-top: 20px;
+  color: var(--festie-gray-650, #6f6f6f);
+  font-family: SUIT Variable;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin-left: 180px;
+`;
+
+const SubMenuShare = styled.div`
+  position: relative;
+  margin-left: 275px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding-top: 20px;
+  color: var(--festie-gray-650, #6f6f6f);
+  font-family: SUIT Variable;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+const SubMenuText = styled(NavLink)`
+  margin-left: 10px;
+  color: var(--festie-gray-650, #6f6f6f);
+  font-family: SUIT Variable;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin-right: 5px;
+  text-decoration: none;
+`;
+const SearchInput = styled.input`
+  outline: none;
+  border: none;
+  border-radius: 20px;
+  width: 100%;
+`;
+const SearchBox = styled.div`
+  padding: 0px 15px;
+  display: flex;
+  width: 340px;
+  height: 40px;
   margin-left: auto;
+  border-radius: 20px;
+  border: 1.5px solid var(--festie-primary-yellow, #ffe500);
+`;
+const Icon = styled.img`
+  cursor: pointer;
+  margin-left: auto;
+  margin-top: 9px;
   width: 24px;
   height: 24px;
 `;
