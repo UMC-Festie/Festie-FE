2edcde2ea5fee20a2f4405ecd2d95605c51f117f
unknown <sohee5143@naver.com>
2023-08-23 03:59:30 +0900

    [fix] 헤더 로그인/회원가입 버튼 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Logo from "../assets/festie_logo.png";
 import SearchIcon from "../assets/search.svg";
 import { NavLink } from "react-router-dom";
@@ -10,6 +10,8 @@ export default function Header() {
   const [isTogetherHovering, setIsTogetherHovering] = useState(0);
   const [inputValue, setInputValue] = useState("");
 
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  const buttonLabel = isLoggedIn !== null ? "My Page" : "로그인/회원가입";
   const activeStyle = {
     borderBottom: "3px solid #FF7A00",
   };
@@ -20,6 +22,16 @@ export default function Header() {
 
   const handleIconClick = () => {
     console.log(inputValue);
+  };
+  const [isInputActive, setIsInputActive] = useState(false); // input 활성화 상태를 추적
+  const borderactiveStyle = {
+    border: `1.5px solid var(--festie-primary-yellow, #ffe500)`, // 활성화 상태일 때 스타일 변경
+  };
+  const handleInputFocus = () => {
+    setIsInputActive(true); // input이 활성화될 때 호출
+  };
+  const handleInputBlur = () => {
+    setIsInputActive(false); // input이 비활성화될 때 호출
   };
 
   return (
@@ -62,7 +74,11 @@ export default function Header() {
         >
           같이 가요
         </Menu>
-        <SearchBox>
+        <SearchBox
+          onFocus={handleInputFocus} // input이 활성화될 때 호출
+          onBlur={handleInputBlur} // input이 비활성화될 때 호출
+          style={isInputActive ? borderactiveStyle : {}} // 활성화 상태에 따라 스타일 변경
+        >
           <SearchInput
             value={inputValue}
             onChange={handleInputChange}
@@ -73,7 +89,9 @@ export default function Header() {
             onClick={handleIconClick}
           ></Icon>
         </SearchBox>
-        <MyButton to="/mypage"> My Page</MyButton>
+        <MyButton to={isLoggedIn ? "/mypage" : "/login"}>
+          {buttonLabel}
+        </MyButton>
       </MenuWrap>
       <SubMenuWrap>
         {isViewHovering ? (
@@ -198,7 +216,6 @@ const SearchBox = styled.div`
   height: 40px;
   margin-left: auto;
   border-radius: 20px;
-  border: 1.5px solid var(--festie-primary-yellow, #ffe500);
 `;
 const Icon = styled.img`
   cursor: pointer;
@@ -210,15 +227,13 @@ const Icon = styled.img`
 
 const MyButton = styled(NavLink)`
   display: flex;
-  width: 80px;
-  padding: 8px 2px;
+  padding: 8px 14px;
   justify-content: center;
   align-items: center;
   border-radius: 18px;
   border: 1px solid var(--festie-gray-500, #b7b7b7);
   flex-shrink: 0;
   color: var(--festie-gray-800, #3a3a3a);
-  font-family: "BreeSerif Regular";
   font-size: 14px;
   font-style: normal;
   font-weight: 400;
@@ -227,4 +242,6 @@ const MyButton = styled(NavLink)`
   margin-left: 24px;
   cursor: pointer;
   text-decoration: none;
+  font-family: ${(props) =>
+    props.isLoggedIn ? "BreeSerif Regular" : "SUIT Variable"};
 `;
